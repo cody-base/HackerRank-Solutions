131f48432ad83c8e334ab42d347dc66fa1bba8d3
cctay <36292382+cctay@users.noreply.github.com>
2019-11-21 20:50:44 -0600

    Update 2D Array - DS.java

--- Java/2D Array - DS.java
+++ Java/2D Array - DS.java
@@ -10,16 +10,16 @@ static int hourglassSum(int[][] arr)
     ArrayList<Integer> hourglass = new ArrayList<Integer>();
         
     // Nested for loop to access both rows and columns
-    for (int i=0;i<arr.length;i++)
+    for (int i = 0; i < arr.length; i++)
     {
-        for (int j=0;j<arr.length;j++)
+        for (int j = 0; j < arr.length; j++)
         {
             // Check if this row has 2 values after the current one and if a row exists 2 rows down
             if (!(j+3 > arr.length) && !(i+3 > arr.length))
             {
                 // If hourglass requirements are met, sum this hourglass and add it to the ArrayList.
-                hourglass.add(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+
-                              arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]);
+                hourglass.add(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] +
+                              arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
             }
         }
     }
